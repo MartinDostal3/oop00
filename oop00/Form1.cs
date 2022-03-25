f6a86858caff6444e67d1456dd42c9d1261f593d
dostalm.04 <dostalm.04@A321-14>
2022-03-25 09:28:18 +0100

    základní rozhraní

---
+++ oop00/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace oop00
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Student std = new Student();
+            Student std = new Student("Lakatoš"); 
+
+            std.OznamkujCJL(3);
+            std.OznamkujMAT(2);
+            std.OznamkujMAT(1);
+           
+            
+
+            double p = std.Prumer();
+            if(p !=-1)
+            {
+                MessageBox.Show(std.GetJmeno() + " má průměr známek: " + p);
+            }
+            else
+            {
+                MessageBox.Show(std.GetJmeno() + " neni klasifikován");
+            }
+            
+
+
+        }
+    }
+}
---
+++ oop00/Student.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop00
+{
+    class Student
+    {
+
+        private string jmeno;
+        private int znamkaCJL;
+        private int znamkaMAT;
+        private int znamkaPRG;
+
+        //public Student()
+        //{
+
+        //}
+        public Student(string jmeno)
+        {
+           this.jmeno = jmeno;
+           
+        }
+        public string GetJmeno()
+        {
+            return jmeno;
+        }
+        
+        public void OznamkujCJL(int znamka)
+        {
+            if (znamka > 0 && znamka < 6) znamkaCJL = znamka;
+
+            return;
+        }
+        public void OznamkujMAT( int znamka )
+        {
+            if (znamka > 0 && znamka < 6) znamkaMAT = znamka;
+
+            return;
+        }
+        
+        public void OznamkujPRG(int znamka)
+        {
+            if (znamka > 0 && znamka < 6) znamkaPRG = znamka;
+
+            return;
+        }
+
+        public double Prumer()
+        {
+            if(znamkaCJL != 0 && znamkaMAT !=0 && znamkaPRG != 0)
+            {
+                return (double)(znamkaCJL + znamkaMAT + znamkaPRG) / 3;
+            }
+            else
+            {
+                return -1;
+            }
+
+            
+        }
+
+
+
+    }
+}
